02b14cbf0badc90c443385eedc712d0198edd419
Samuel Pagenkopf <ssaammp@gmail.com>
2018-04-07 01:06:16 -0500

    trashed old assembler, made tokenizer

--- assem.lua
+++ assem.lua
@@ -3,143 +3,56 @@ local ops = require "ops"
 local assem = {}
 
 assem.assemble = function (infile, outfile)
-    local lexed = assem.lex(infile)
-    assem.parse(lexed)
-    --  local binary = assem.link(lexed)
+    local tok = assem.tokenize(infile)
 end
 
--- turn a file into a list of lines that should be easy to parse
-assem.lex = function (infile)
-    local token_sig = "[%a_][%w_]*"
-    local lexed = {}
-
+assem.tokenize = function (infile)
+    local lines = {}
     for line in infile:lines() do
-        local len = #lexed+1
-        lexed[len] = {}
-
-        line = line:gsub(";.*", "")
-
-        -- empty line
-        if line:match("^%s*$") then
-            goto continue
-        end
-
-        -- basic assignment, not considered part of binary
-        local lvalue, rvalue = line:match("(" .. token_sig .. ")%s*=%s*([^%s]*)")
-        if lvalue then
-            lexed[len] = {
-                kind = "assignment",
-                lvalue = lvalue,
-                rvalue = rvalue,
-            }
-            goto continue
-        end
-
-        -- label must be at start of line (no whitespace), colon optional
-        -- control flow labels are stored alongside the data
-        local label, lend = line:find("^(" .. token_sig .. ")")
-        if label then
-            lexed[len].label = line:sub(label, lend)
-        end
-
-        -- opcode must have whitespace before it
-        -- opcode starting with . indicates directive
-        local opname, oend = line:find("[.]?%a+", lend and lend+1)
-        if opname then
-            -- arguments are a list of commas which can include 'quotes'
-            lexed[len] = {
-                kind = "instruction",
-                opname = line:sub(opname, oend):lower()
-            }
-            local args = {}
-            local pos = oend+1
-            while true do
-                -- quotes get priority over other arguments
-                local quote, qend = line:find("%b''", pos)
-                local normal, nend = line:find("[^%s,]+", pos)
-                if quote then
-                    args[#args+1] = line:sub(quote, qend)
-                    pos = qend+1
-                elseif normal then
-                    args[#args+1] = line:sub(normal, nend)
-                    pos = nend+1
-                else
-                    break
-                end
-            end
-            lexed[len].args = args
-
-            --      io.write(lexed[len].opname, " ", table.concat(lexed[len].args, ", "), "\n")
-        end
-        ::continue::
+        lines[#lines+1] = assem.tokenize_line(line)
     end
-
-    return lexed
+    return lines
 end
 
--- The parser just adds a .words = {...} to lexed data
-assem.parse = function (lexed)
-    local get_amode = function (arg)
-        if not arg then
-            return 0
-        end
-        local amode
-        local ptr
-
-        -- special arguments seen as registers
-        local regs = {
-            a = 0,
-            b = 1,
-            c = 2,
-            d = 3,
-            pc = 4,
-            sp = 5,
-        }
-        -- for *x "pointer" register syntax
-        if arg:sub(1,1) == "*" then
-            arg = arg:sub(2)
-            ptr = true
-        end
-        local reg = regs[arg]
-        if reg then
-            if ptr then
-                return reg + 6
+assem.tokenize_line = function (line)
+    line = line
+        :gsub(";.*", "") -- strip comments
+    if line:match("^%s*$") then
+        return nil
+    end
+    local tokens = {}
+    -- possible tokens
+    local sig_def = {
+        "%b''",    -- quote
+        '%b""',    -- quote
+        "%S",      -- single symbol
+        "[%w_]+",  -- word or number
+    }
+    local pos = 0
+    local token_pos, token_end
+    repeat
+        -- locate the earliest valid token, otherwise longest
+        token_pos, token_end = nil, nil
+        for _, sig in ipairs(sig_def) do
+            local try_pos, try_end = line:find(sig, pos+1)
+            -- skip past escaped quotes
+            while try_pos and line:find("^[^\\]\\['\"]", try_end-2) do
+                try_end = line:find(line:sub(try_end, try_end), try_end+1)
             end
-            return reg
+            if try_pos and ((not token_pos) or
+                    try_pos < token_pos or
+                    (try_pos == token_pos and try_end > token_end))
+            then
+                token_pos, token_end = try_pos, try_end
+            end
         end
-        -- for regular values (which must be parsed)
-        if arg:sub(1,1) == "#" then
-            return 0xC, assem.parse_expr(arg:sub(2))
-        else
-            return 0xD, assem.parse_expr(arg)
+        if token_pos then
+            pos = token_end
+            tokens[#tokens+1] = line:sub(token_pos, token_end)
         end
-    end
-
-    for i, line in ipairs(lexed) do
-        if line.kind ~= "instruction" then
-            goto continue
-        end
-        local op = ops.by_name[line.opname]
-
-        if op then
-            local amode = 0
-            local m1, v1 = get_amode(line.args[1])
-            local m2, v2 = get_amode(line.args[2])
-            line.words = {op.code * 0x1000 + m1 * 0x10 + m2}
-            -- if no values (i.e. registers), nothing is added
-            table.insert(line.words, v1)
-            table.insert(line.words, v2)
-            --      print(line.opname, ("%02x"):format(amode))
-            for _,v in ipairs(line.words) do io.write(("%04x "):format(v)) end print()
-        else
-            -- handle directive
-        end
-        ::continue::
-    end
-end
-
-assem.parse_expr = function (expr)
-    return 0
+    until not token_pos
+    print(table.concat(tokens, ' | '))
+    return tokens
 end
 
 return assem
