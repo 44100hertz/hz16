ff460e6b598d5c0197917b22634c43a65bf1d27d
Samuel Pagenkopf <ssaammp@gmail.com>
2018-04-09 01:08:43 -0500

    put lexer into another file

--- assem.lua
+++ assem.lua
@@ -1,4 +1,5 @@
 local ops = require "ops"
+local lex = require "lex"
 
 local assem = {}
 
@@ -11,7 +12,7 @@ assem.assemble_and_write = function (inpath, outpath)
 end
 
 assem.assemble = function (infile)
-    local program = assem.tokenize(infile)
+    local program = lex.tokenize(infile)
     assem.parse(program)
     return program
 end
@@ -24,62 +25,6 @@ assem.write_bin = function (bin, outpath)
                           bit.band(0xff, bit.rshift(word, 8))))
     end
     outfile:close()
-end
-
-assem.tokenize = function (infile)
-    local lines = {}
-    for line in infile:lines() do
-        lines[#lines+1] = assem.tokenize_line(line)
-    end
-    return lines
-end
-
-assem.tokenize_line = function (line)
-    line = line:gsub(";.*", "") -- strip comments
-    if line:match("^%s*$") then
-        return nil
-    end
-    local tokens = {}
-    tokens.raw_line = line
-    -- possible tokens
-    local sig_def = {
-        "%b''",    -- quote
-        '%b""',    -- quote
-        "%S",      -- single symbol
-        "[%w_]+",  -- word or number
-    }
-    local pos = 0
-    repeat
-        -- locate the earliest valid token, otherwise longest
-        local token_pos, token_end = nil, nil
-        for _, sig in ipairs(sig_def) do
-            local try_pos, try_end = line:find(sig, pos+1)
-            -- skip past escaped quotes
-            -- and the dreaded triple backslash (best solution for now)
-            while try_pos and (line:find("^[^\\]\\['\"]", try_end-2) or
-                               line:find("^\\\\\\['\"]", try_end-3))
-            do
-                local new_end = line:find(line:sub(try_end, try_end), try_end+1)
-                if new_end then
-                    try_end = new_end
-                else
-                    error("incomplete string")
-                end
-            end
-            if try_pos and ((not token_pos) or
-                    try_pos < token_pos or
-                    (try_pos == token_pos and try_end > token_end))
-            then
-                token_pos, token_end = try_pos, try_end
-            end
-        end
-        if token_pos then
-            pos = token_end
-            tokens[#tokens+1] = line:sub(token_pos, token_end)
-        end
-    until not token_pos
---    print(table.concat(tokens, ' | '))
-    return tokens
 end
 
 assem.parse = function (lines, defined)
@@ -160,7 +105,6 @@ assem.split_by_comma = function (line, pos)
     until not line[pos]
     return args
 end
-
 
 assem.parse_arg = function (arg, defined)
     if not arg then
---
+++ lex.lua
@@ -0,0 +1,59 @@
+local lex = {}
+
+lex.tokenize = function (infile)
+    local lines = {}
+    for line in infile:lines() do
+        lines[#lines+1] = lex.tokenize_line(line)
+    end
+    return lines
+end
+
+lex.tokenize_line = function (line)
+    line = line:gsub(";.*", "") -- strip comments
+    if line:match("^%s*$") then
+        return nil
+    end
+    local tokens = {}
+    tokens.raw_line = line
+    -- possible tokens
+    local sig_def = {
+        "%b''",    -- quote
+        '%b""',    -- quote
+        "%S",      -- single symbol
+        "[%w_]+",  -- word or number
+    }
+    local pos = 0
+    repeat
+        -- locate the earliest valid token, otherwise longest
+        local token_pos, token_end = nil, nil
+        for _, sig in ipairs(sig_def) do
+            local try_pos, try_end = line:find(sig, pos+1)
+            -- skip past escaped quotes
+            -- and the dreaded triple backslash (best solution for now)
+            while try_pos and (line:find("^[^\\]\\['\"]", try_end-2) or
+                               line:find("^\\\\\\['\"]", try_end-3))
+            do
+                local new_end = line:find(line:sub(try_end, try_end), try_end+1)
+                if new_end then
+                    try_end = new_end
+                else
+                    error("incomplete string")
+                end
+            end
+            if try_pos and ((not token_pos) or
+                    try_pos < token_pos or
+                    (try_pos == token_pos and try_end > token_end))
+            then
+                token_pos, token_end = try_pos, try_end
+            end
+        end
+        if token_pos then
+            pos = token_end
+            tokens[#tokens+1] = line:sub(token_pos, token_end)
+        end
+    until not token_pos
+--    print(table.concat(tokens, ' | '))
+    return tokens
+end
+
+return lex
